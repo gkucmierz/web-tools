056aa3763889cc647e4807a3ddb0a9b85534add5
Grzegorz Kucmierz <gkucmierz@gmail.com>
2019-05-18 00:32:59 +0200

    add reset-css; add toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -13,7 +15,9 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
