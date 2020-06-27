0e4a94ae8bf4ba90f4d54b483c403e05775c7e85
Grzegorz Kucmierz <gkucmierz@gmail.com>
2020-06-28 01:44:52 +0200

    fix qr scanner component

--- src/app/qr-scanner/qr-scanner.component.ts
+++ src/app/qr-scanner/qr-scanner.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 
-import {QrScannerComponent as QrScanner} from 'angular2-qrscanner';
+import { QrScannerComponent as QrScanner } from 'angular2-qrscanner';
 
 @Component({
   selector: 'app-qr-scanner',
   templateUrl: './qr-scanner.component.html',
-  styleUrls: ['./qr-scanner.component.scss']
+  styleUrls: ['./qr-scanner.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class QrScannerComponent implements OnInit {
-  @ViewChild(QrScanner) qrScannerComponent: QrScanner;
+  @ViewChild(QrScanner) qrScanner: QrScanner;
 
   lastScan: string;
   scanned: string[] = [];
@@ -25,39 +26,54 @@ export class QrScannerComponent implements OnInit {
 
   changeCamera(cameraIdx) {
     this.cameraIdx = cameraIdx;
-    this.qrScannerComponent.chooseCamera.next(this.cameras[this.cameraIdx]);
+    this.qrScanner.chooseCamera.next(this.cameras[this.cameraIdx]);
+  }
+
+  wait(prop, cb) {
+    const DELAY = 50;
+    const loop = () => {
+      if (prop in this) {
+        cb(this[prop]);
+      } else {
+        setTimeout(loop, DELAY);
+      }
+    };
+    loop();
   }
 
   ngOnInit() {
-    this.qrScannerComponent.getMediaDevices().then(devices => {
-      const videoDevices: MediaDeviceInfo[] = [];
-      let choosenDev;
+    this.wait('qrScanner', qrScanner => {
+      this.qrScanner.getMediaDevices().then(devices => {
+        const videoDevices: MediaDeviceInfo[] = [];
+        let choosenDev;
 
-      for (const device of devices) {
-        if (device.kind.toString() === 'videoinput') {
-          videoDevices.push(device);
-        }
-      }
-
-      choosenDev = videoDevices[0];
-      if (videoDevices.length > 0) {
-        for (const dev of videoDevices) {
-          if (dev.label.includes('front')) {
-            choosenDev = dev;
-            break;
+        for (const device of devices) {
+          if (device.kind.toString() === 'videoinput') {
+            videoDevices.push(device);
           }
         }
-      }
 
-      this.changeCamera(this.cameras.indexOf(choosenDev));
-    });
+        choosenDev = videoDevices[0];
+        if (videoDevices.length > 0) {
+          for (const dev of videoDevices) {
+            if (dev.label.includes('front')) {
+              choosenDev = dev;
+              break;
+            }
+          }
+        }
 
-    this.qrScannerComponent.capturedQr.subscribe(result => {
-      if (this.lastScan !== result) {
-        this.lastScan = result;
-        this.scanned.push(result);
-      }
+        this.changeCamera(this.cameras.indexOf(choosenDev));
+      });
+
+      this.qrScanner.capturedQr.subscribe(result => {
+        if (this.lastScan !== result) {
+          this.lastScan = result;
+          this.scanned.push(result);
+        }
+      });
     });
+      
   }
 
 }
