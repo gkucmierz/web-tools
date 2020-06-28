c443c0bcc8523b90045ee318769fec91e38c9f66
Grzegorz Kucmierz <gkucmierz@gmail.com>
2020-06-28 11:56:31 +0200

    removed broken tool

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { QrScannerComponent } from './qr-scanner/qr-scanner.component';
-import { JsBeautifierComponent } from './js-beautifier/js-beautifier.component';
 
 const routes: Routes = [
   { path: 'qr-scanner', component: QrScannerComponent },
-  { path: 'js-beautifier', component: JsBeautifierComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,31 +5,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatButtonModule} from '@angular/material/button';
-import {MatListModule} from '@angular/material/list';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { NgQrScannerModule } from 'angular2-qrscanner';
-import { MatCodemirrorModule } from '@webacad/ng-mat-codemirror';
 
 import { MenuComponent } from './menu/menu.component';
 import { QrScannerComponent } from './qr-scanner/qr-scanner.component';
-import { JsBeautifierComponent } from './js-beautifier/js-beautifier.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     QrScannerComponent,
-    JsBeautifierComponent
   ],
   imports: [
     BrowserModule,
@@ -46,7 +43,6 @@ import { JsBeautifierComponent } from './js-beautifier/js-beautifier.component';
     MatFormFieldModule,
     MatInputModule,
     NgQrScannerModule,
-    MatCodemirrorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
