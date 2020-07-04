0aaf1638a8ca382ee47233ea4a3957b80523c4f5
Grzegorz Kucmierz <gkucmierz@gmail.com>
2020-07-04 21:04:52 +0200

    main: add more description

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -11,6 +11,31 @@ export class MainComponent {
 # Web Tools
 
 All kinds of web tools included in one compact web-app
+
+### Development
+- [fork](https://github.com/gkucmierz/web-tools)
+- Clone
+- \`\`npm install\`\`
+- \`\`npm run start\`\`
+- Commit
+- PR (Pull Request)
+
+### Issues
+
+Issues, enhancements, bugs etc.
+
+- [Issues](https://github.com/gkucmierz/web-tools/issues)
+- [New Issue](https://github.com/gkucmierz/web-tools/issues/new)
+
+### Thanks to
+
+- [Angular](https://github.com/angular) - Great framework for building web-apps
+- [Angular Material](https://material.angular.io/) - Material Design for angular, great UI
+- [TypeScript](https://www.typescriptlang.org) - better JavaScript
+- And many others…
+
+### Author
+[Grzegorz Kućmierz](https://github.com/gkucmierz) - Experienced Front-end Developer
 `;
   html: string;
 
