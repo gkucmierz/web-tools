15a594b83d51d969bbd9c9187f0c04a302969a96
Grzegorz Kucmierz <gkucmierz@gmail.com>
2020-07-12 22:50:18 +0200

    base64: add unicode support

--- src/app/tools/base64/base64.component.ts
+++ src/app/tools/base64/base64.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+/**
+ * ASCII to Unicode (decode Base64 to original data)
+ * @param {string} b64
+ * @return {string}
+ */
+const atou = (b64: string): string => decodeURIComponent(escape(atob(b64)));
+
+/**
+ * Unicode to ASCII (encode data to Base64)
+ * @param {string} data
+ * @return {string}
+ */
+const utoa = (data: string): string => btoa(unescape(encodeURIComponent(data)));
+
 @Component({
   selector: 'app-base64',
   templateUrl: './base64.component.html',
@@ -12,13 +26,13 @@ export class Base64Component implements OnInit {
 
   encode(): void {
     try {
-      this.text = btoa(this.text);
+      this.text = utoa(this.text);
     } catch (e) { }
   }
 
   decode(): void {
     try {
-      this.text = atob(this.text);
+      this.text = atou(this.text);
     } catch (e) { }
   }
 
