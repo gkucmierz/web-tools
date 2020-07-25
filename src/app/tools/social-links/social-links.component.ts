1bdb14176bc5c71a87d1b10bf26a69a02905aaa2
Grzegorz Kucmierz <gkucmierz@gmail.com>
2020-07-25 12:43:48 +0200

    tool: social-links - add more features

--- src/app/tools/social-links/social-links.component.ts
+++ src/app/tools/social-links/social-links.component.ts
@@ -1,35 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { SocialLinks } from 'social-links';
 
+const SOCIALS = [
+  'linkedin', 'twitter', 'facebook', 'youtube', 'twitch',
+  'instagram', 'patreon', 'github', 'medium', 'dribbble', 'behance'
+];
+
 @Component({
   selector: 'app-social-links',
   templateUrl: './social-links.component.html',
   styleUrls: ['./social-links.component.scss']
 })
 export class SocialLinksComponent implements OnInit {
-  socials = [
-    'linkedin', 'twitter', 'facebook', 'youtube', 'twitch',
-    'instagram', 'patreon', 'github', 'medium', 'dribbble', 'behance'
-  ];
+  socials = SOCIALS;
+  social = SOCIALS[0];
+  value = '';
   result = { };
-  social = '';
+  trimInput = true;
+  allowQueryParams = false;
 
   constructor() { }
 
-  onSearchChange(value, social): void {
+  onChange(): void {
     this.result = {};
-    const sl = new SocialLinks();
-    const isValid = sl.isValid(social, value);
-    try {
-      const sanitized = sl.sanitize(social, value);
-      const profileId = sl.getProfileId(social, value);
-      this.result = { [social]: { isValid, profileId, sanitized } };
-    } catch (e) {
-      this.result = { [social]: { isValid } };
-    }
 
+    const sl = new SocialLinks({
+      trimInput: this.trimInput,
+      allowQueryParams: this.allowQueryParams,
+    });
+
+    const isValid = sl.isValid(this.social, this.value);
+
+    try {
+      const sanitized = sl.sanitize(this.social, this.value);
+      const profileId = sl.getProfileId(this.social, this.value);
+      this.result = { [this.social]: { isValid, profileId, sanitized } };
+    } catch (e) {
+      this.result = { [this.social]: { isValid } };
+    }
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.onChange();
+  }
 
 }
